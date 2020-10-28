e89150ec0f1573ef611ac342bb385e8193dfba7b
Yoni <46503638+yoniakabecky@users.noreply.github.com>
2020-10-28 10:11:36 -0700

    Setup Graphql (#1)
    * Add Apollo Server
    
    * Add type-graphql and resolver

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
 import express from "express";
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import resolvers from "./resolvers";
 
 const main = async () => {
-    const app = express();
+  const app = express();
 
-    app.get("/", (_, res) => res.send("hello"));
+  app.get("/", (_, res) => res.send("hello"));
 
-    app.listen(4000, () => {
-        console.log("express server started 🚀")
-    })
+  await createConnection();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({ resolvers })
+  })
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started 🚀 http://localhost:4000/graphql")
+  })
 };
 
 main();
