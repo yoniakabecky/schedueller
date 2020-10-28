164313b8808e33c2b9e17c9f4974a84deaaab37a
Yoni <yoniakabecky@gmail.com>
2020-10-27 21:55:56 -0700

    Add Apollo Server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,33 @@
 import "reflect-metadata";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { createConnection } from "typeorm";
 
 const main = async () => {
-    const app = express();
+  const app = express();
 
-    app.get("/", (_, res) => res.send("hello"));
+  app.get("/", (_, res) => res.send("hello"));
 
-    app.listen(4000, () => {
-        console.log("express server started 🚀")
-    })
+  await createConnection();
+
+  const apolloServer = new ApolloServer({
+    typeDefs: `
+      type Query {
+        hello: String!
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => "hello world",
+      }
+    }
+  })
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("express server started 🚀")
+  })
 };
 
 main();
