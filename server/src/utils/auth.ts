9f8daf9ae9c063e528dabec5707a333847c02060
Yoni <yoniakabecky@gmail.com>
2020-11-29 12:53:12 -0800

    Create a server dir

---
+++ server/src/utils/auth.ts
@@ -0,0 +1,21 @@
+import { verify } from "jsonwebtoken";
+import { MiddlewareFn } from "type-graphql";
+import { MyContext } from "./myContext";
+
+export const authenticated: MiddlewareFn<MyContext> = ({ context }, next) => {
+  const authorization = context.req.headers["authorization"];
+
+  if (!authorization) throw new Error("Not authenticated");
+
+  try {
+    const token = authorization.split(" ")[1];
+    const payload = verify(token, process.env.ACCESS_TOKEN_SECRET!);
+
+    context.payload = payload as any;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Not authenticated");
+  }
+
+  return next();
+}
