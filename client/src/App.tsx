34bd94927ab9ce921df66c7a9422fce8735cd33a
Yoni <yoniakabecky@gmail.com>
2021-01-05 12:42:41 -0800

    Login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,10 @@
 import { ApolloProvider } from "@apollo/client";
 import React from "react";
-import { GET_USERS } from "./graphql/queries";
 import { useAppApolloClient } from "./hooks/useAppApolloClient";
 import Routes from "./Routes";
 
 function App() {
   const client = useAppApolloClient();
-
-  client
-    .query({
-      query: GET_USERS,
-    })
-    .then((result) => console.log(result));
 
   return (
     <ApolloProvider client={client}>
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,24 +1,65 @@
+import { useMutation } from "@apollo/client";
 import React, { ReactElement } from "react";
+import { LOGIN } from "../graphql/mutations";
 
 interface Props {}
 
+type LoginInput = {
+  data: {
+    email: string;
+    password: string;
+  };
+};
+
+type LoginResponse = {
+  token: string;
+  isCompany: boolean;
+};
+
 export default function Login(props: Props): ReactElement {
-  const handleSubmit = () => {
-    console.log("submit");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const [login, { loading, error }] = useMutation<LoginResponse, LoginInput>(
+    LOGIN
+  );
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (email && password) {
+      await login({
+        variables: { data: { email, password } },
+      });
+    }
   };
 
   return (
     <div>
       <h1>Login</h1>
 
+      {error && <p>Error</p>}
+
       <form onSubmit={handleSubmit}>
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <br />
         <br />
 
         <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <br />
         <br />
 
