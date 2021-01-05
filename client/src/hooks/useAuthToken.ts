8013eceda10c9764e9924c802454bb427e52ce12
Yoni <yoniakabecky@gmail.com>
2021-01-05 14:21:51 -0800

    Logout

--- client/src/hooks/useAuthToken.ts
+++ client/src/hooks/useAuthToken.ts
@@ -1,3 +1,4 @@
+import { useApolloClient } from "@apollo/client";
 import { useCookies } from "react-cookie";
 
 const TOKEN_NAME = "schedulerToken";
@@ -10,4 +11,16 @@ export const useAuthToken = () => {
   const removeAuthToken = () => removeCookie(TOKEN_NAME);
 
   return [cookies[TOKEN_NAME], setAuthToken, removeAuthToken];
+};
+
+export const useLogout = () => {
+  const [, , removeAuthToken] = useAuthToken();
+  const apolloClient = useApolloClient();
+
+  const logout = async () => {
+    await apolloClient.clearStore();
+    removeAuthToken();
+  };
+
+  return logout;
 };
